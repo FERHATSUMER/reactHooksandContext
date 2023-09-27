b78be7dca7a1e833095eacd6b776795719a11dbe
FERHATSUMER <sumerf39@gmail.com>
2023-09-27 15:14:19 +0300

    reactHooks

--- mainpage/src/pages/home/index.js
+++ mainpage/src/pages/home/index.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>Home</h1>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
