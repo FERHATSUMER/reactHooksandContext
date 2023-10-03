c9eb7d54e2f4779dc8a2bfdd46e2dd53bed107e8
FERHATSUMER <sumerf39@gmail.com>
2023-10-04 01:01:21 +0300

    reactHooks

--- mainpage/src/components/navbar/index.js
+++ mainpage/src/components/navbar/index.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
 const Navbar = () => {
   return (
-    <nav className='navbar-container'>
-      <h3 className='navbar-logo'>Logo</h3> 
-        <ul className='navbar-ul'>
-            <li>About</li>
-            <li>Member</li>
+    <nav className="navbar-container">
+      <div className="navbar-right">
+        <img
+          alt="logo"
+          className="navbar-logo"
+          src="https://media.licdn.com/dms/image/D4D03AQFMfnp5H1M-vA/profile-displayphoto-shrink_800_800/0/1667210919849?e=1701907200&v=beta&t=LpKqBog9O5FAQFtfDZ_a8iK0CqgCbDuQicq45YK5DNY"
+        />
+        <h2 className="navbar-title">Ferhat Sümer</h2>
+      </div>
+      <div className="navbar-left">
+        <ul className="navbar-ul">
+          <li>About</li>
+          <li>Member</li>
+          <li>Project</li>
+          <li>Blog</li>
         </ul>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
