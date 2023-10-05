411814a1c185911e0480a09b2e566837638f36a2
FERHATSUMER <sumerf39@gmail.com>
2023-10-05 17:57:46 +0300

    react-Hooks

--- mainpage/src/pages/member/index.js
+++ mainpage/src/pages/member/index.js
@@ -1,11 +1,34 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 const Member = () => {
+  useEffect(() => {
+    takeData();
+  }, []);
+
+  const [data, setData] = useState([]);
+
+  const takeData = async () => {
+    const data = await fetch("https://jsonplaceholder.typicode.com/users");
+    const res = await data.json();
+    //console.log(res)
+    setData(res);
+  };
   return (
     <div>
       <h1>Member</h1>
+      {data.map((i) => {
+        return (
+          <div key={i.id}>
+            <h3>
+              <Link to={`/about${i.email }`}>
+              {i.name}
+              </Link>
+            </h3>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Member
+export default Member;
