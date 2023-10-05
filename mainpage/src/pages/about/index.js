938590bb6e31bf7b901dbaa7b8f9946025867b56
FERHATSUMER <sumerf39@gmail.com>
2023-10-05 18:01:19 +0300

    reactHooks

--- mainpage/src/pages/about/index.js
+++ mainpage/src/pages/about/index.js
@@ -1,14 +1,11 @@
-import React,{useEffect} from 'react'
+import React from 'react'
 
-const About = ({match}) => {
-  useEffect(() => {
-    console.log(match)
-  }, [])
+const About = () => {
+
   return (
     <div>
       <h1>About</h1>
-      <hr/>
-      <h3>{match.params.email}</h3>
+
     </div>
   )
 }
